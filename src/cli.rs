416c0e50747be168478e43140204eeb42cee51e9
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-10-11 06:21:47 -0700

    Run rustfmt on code

--- src/cli.rs
+++ src/cli.rs
@@ -6,25 +6,23 @@ license: CC0 1.0 Universal https://creativecommons.org/publicdomain/zero/1.0/
 
 */
 
-
 use anyhow;
 use clap::{Parser, ValueEnum};
 
-
 #[derive(Clone, Parser, ValueEnum, Debug)]
-pub(crate) enum OutputMode
-{
+pub(crate) enum OutputMode {
     CSV,
     Default,
     Regular,
 }
 
-
 #[derive(Parser, Debug)]
 #[command(author = "Christopher Eades", version = "1.0", about = "A rust ICMP echo request (ping) utility.", long_about = None)]
-pub(crate) struct Args
-{
-    #[arg(required = true, help = "Host (or address) or CSV format: host,count,interval (implies --output=csv)")]
+pub(crate) struct Args {
+    #[arg(
+        required = true,
+        help = "Host (or address) or CSV format: host,count,interval (implies --output=csv)"
+    )]
     pub host_or_args: String,
 
     #[arg(value_enum, short, long, default_value_t = OutputMode::Default, help = "Output mode; defaults to Regular. --quiet disables output entirely.")]
@@ -46,46 +44,64 @@ pub(crate) struct Args
     pub timeout: u64,
 
     // size of the packet //
-    #[arg(short, long, default_value_t = 32, help = "Size of the ICMP echo data to send.")]
+    #[arg(
+        short,
+        long,
+        default_value_t = 32,
+        help = "Size of the ICMP echo data to send."
+    )]
     pub size: usize,
-    
+
     // quiet or not //
-    #[arg(short, long, default_value_t = false, help = "Program won't output any regular output.")]
+    #[arg(
+        short,
+        long,
+        default_value_t = false,
+        help = "Program won't output any regular output."
+    )]
     pub quiet: bool,
 }
 
 // some support for the cli parser //
 
-
-pub fn count_range(count_parsed: &str) -> Result<u64, anyhow::Error>
-{
+pub fn count_range(count_parsed: &str) -> Result<u64, anyhow::Error> {
     // require the count to be from 1 to 10 //
-    match count_parsed.parse()
-    {
-        Ok(count) if count == 0 => Err(anyhow::anyhow!( "The count must be at least 1: {}", count )),
+    match count_parsed.parse() {
+        Ok(count) if count == 0 => Err(anyhow::anyhow!("The count must be at least 1: {}", count)),
         Ok(count) if count > 0 && count <= 10 => Ok(count),
-        _ => Err(anyhow::anyhow!( "The count must be a value between 1 and 10: {}", count_parsed )),
+        _ => Err(anyhow::anyhow!(
+            "The count must be a value between 1 and 10: {}",
+            count_parsed
+        )),
     }
 }
 
-pub fn interval_range(interval_parsed: &str) -> Result<u64, anyhow::Error>
-{
+pub fn interval_range(interval_parsed: &str) -> Result<u64, anyhow::Error> {
     // require the interval to be from 1 to 1000ms... basically no flooding and no super long requests //
-    match interval_parsed.parse()
-    {
-        Ok(interval) if interval == 0 => Err(anyhow::anyhow!( "The interval must be at least 1ms: {}", interval )),
+    match interval_parsed.parse() {
+        Ok(interval) if interval == 0 => Err(anyhow::anyhow!(
+            "The interval must be at least 1ms: {}",
+            interval
+        )),
         Ok(interval) if interval > 0 && interval <= 1000 => Ok(interval),
-        _ => Err(anyhow::anyhow!( "The interval must be a value between 1 and 1000ms: {}", interval_parsed )),
+        _ => Err(anyhow::anyhow!(
+            "The interval must be a value between 1 and 1000ms: {}",
+            interval_parsed
+        )),
     }
 }
 
-pub fn timeout_range(timeout_parsed: &str) -> Result<u64, anyhow::Error>
-{
+pub fn timeout_range(timeout_parsed: &str) -> Result<u64, anyhow::Error> {
     // require the timeout to be from 1 to 10000ms //
-    match timeout_parsed.parse()
-    {
-        Ok(timeout) if timeout == 0 => Err(anyhow::anyhow!( "The timeout must be at least 1ms: {}", timeout )),
+    match timeout_parsed.parse() {
+        Ok(timeout) if timeout == 0 => Err(anyhow::anyhow!(
+            "The timeout must be at least 1ms: {}",
+            timeout
+        )),
         Ok(timeout) if timeout > 0 && timeout <= 10000 => Ok(timeout),
-        _ => Err(anyhow::anyhow!( "The timeout must be a value between 1 and 10000ms: {}", timeout_parsed )),
+        _ => Err(anyhow::anyhow!(
+            "The timeout must be a value between 1 and 10000ms: {}",
+            timeout_parsed
+        )),
     }
 }
--- src/ipv4/icmp.rs
+++ src/ipv4/icmp.rs
@@ -6,13 +6,11 @@ license: CC0 1.0 Universal https://creativecommons.org/publicdomain/zero/1.0/
 
 */
 
-
 use anyhow;
 use bincode;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use socket2::SockAddr;
 use std::net::IpAddr;
-
 
 // ICMPv4 header size //
 pub const ICMPV4_PKT_BUF_SIZE: usize = 8;
@@ -22,10 +20,8 @@ pub const D_IPV4_ICMP_TYPE_UNREACHABLE: u8 = 0x03;
 pub const D_IPV4_ICMP_TYPE_ECHO_REQUEST: u8 = 0x08;
 pub const D_IPV4_ICMP_TYPE_EXCEEDED: u8 = 0x0B;
 
-
 #[derive(Serialize, Deserialize, Debug)]
-pub struct IcmpPacketHeaderV4
-{
+pub struct IcmpPacketHeaderV4 {
     pub r#type: u8,
     pub code: u8,
     pub checksum: u16,
@@ -33,27 +29,27 @@ pub struct IcmpPacketHeaderV4
     pub sequence: u16,
 }
 
-
-pub fn ipv4_icmp_create_echo_request(identifier: u16, sequence: u16, data_size: usize) -> anyhow::Result<Vec<u8>>
-{
+pub fn ipv4_icmp_create_echo_request(
+    identifier: u16,
+    sequence: u16,
+    data_size: usize,
+) -> anyhow::Result<Vec<u8>> {
     // create the icmp echo request //
     let mut buf: Vec<u8> = vec![];
-    buf.resize( ICMPV4_PKT_BUF_SIZE + data_size, 0x00 );
+    buf.resize(ICMPV4_PKT_BUF_SIZE + data_size, 0x00);
 
     // write header //
-    let header = IcmpPacketHeaderV4
-    {
+    let header = IcmpPacketHeaderV4 {
         r#type: D_IPV4_ICMP_TYPE_ECHO_REQUEST,
         code: 0,
         checksum: 0,
         identifier,
         sequence,
     };
-    bincode::serialize_into( &mut buf[0..], &header )?;
+    bincode::serialize_into(&mut buf[0..], &header)?;
 
     // write the data: ABCDEF... ABCDEF... //
-    for n in 0..data_size - 1
-    {
+    for n in 0..data_size - 1 {
         buf[ICMPV4_PKT_BUF_SIZE + n] = ((n % 26) + 65) as u8;
     }
 
@@ -64,46 +60,70 @@ pub fn ipv4_icmp_create_echo_request(identifier: u16, sequence: u16, data_size: 
     Ok(buf)
 }
 
-
-pub(crate) fn ipv4_handle_icmp_packet(remote_sock: &SockAddr, ip: &crate::ipv4::Ipv4PacketHeaderV4, buf: &Vec<u8>, program_state: &mut crate::IcmpProgramState) -> anyhow::Result<()>
-{
+pub(crate) fn ipv4_handle_icmp_packet(
+    remote_sock: &SockAddr,
+    ip: &crate::ipv4::Ipv4PacketHeaderV4,
+    buf: &Vec<u8>,
+    program_state: &mut crate::IcmpProgramState,
+) -> anyhow::Result<()> {
     // handle incoming IPv4 ICMP packet //
     let icmp: IcmpPacketHeaderV4 = bincode::deserialize(buf.as_slice())?;
-    log::debug!( "got icmp packet: {:?}", &icmp );
-    match icmp.r#type
-    {
-        D_IPV4_ICMP_TYPE_ECHO_REPLY => ipv4_handle_icmp_echo_reply( remote_sock, ip, buf, &icmp, program_state ),
-        D_IPV4_ICMP_TYPE_UNREACHABLE | D_IPV4_ICMP_TYPE_EXCEEDED => ipv4_handle_icmp_error( remote_sock, ip, buf, &icmp, program_state ),
-        _ =>
-        {
+    log::debug!("got icmp packet: {:?}", &icmp);
+    match icmp.r#type {
+        D_IPV4_ICMP_TYPE_ECHO_REPLY => {
+            ipv4_handle_icmp_echo_reply(remote_sock, ip, buf, &icmp, program_state)
+        }
+        D_IPV4_ICMP_TYPE_UNREACHABLE | D_IPV4_ICMP_TYPE_EXCEEDED => {
+            ipv4_handle_icmp_error(remote_sock, ip, buf, &icmp, program_state)
+        }
+        _ => {
             let remote_addr = remote_sock.as_socket_ipv4().unwrap();
-            Err(anyhow::anyhow!( "got unexpected ICMP packet type {} code {} from: {}", icmp.r#type, icmp.code, remote_addr ))
-        },
+            Err(anyhow::anyhow!(
+                "got unexpected ICMP packet type {} code {} from: {}",
+                icmp.r#type,
+                icmp.code,
+                remote_addr
+            ))
+        }
     }
 }
 
 pub(crate) fn ipv4_handle_icmp_echo_reply(
-  remote_sock: &SockAddr,
-  ip: &crate::ipv4::Ipv4PacketHeaderV4,
-  buf: &Vec<u8>,
-  icmp: &IcmpPacketHeaderV4,
-  program_state: &mut crate::IcmpProgramState) -> anyhow::Result<()>
-{
+    remote_sock: &SockAddr,
+    ip: &crate::ipv4::Ipv4PacketHeaderV4,
+    buf: &Vec<u8>,
+    icmp: &IcmpPacketHeaderV4,
+    program_state: &mut crate::IcmpProgramState,
+) -> anyhow::Result<()> {
     let remote_addr = remote_sock.as_socket_ipv4().unwrap();
-    if icmp.identifier != program_state.identifier
-    {
-        return Err(anyhow::anyhow!( "got unexpected ICMP echo identifier {} (was expecting {}) sequence={} from: {}", icmp.identifier, program_state.identifier, icmp.sequence, remote_addr ));
+    if icmp.identifier != program_state.identifier {
+        return Err(anyhow::anyhow!(
+            "got unexpected ICMP echo identifier {} (was expecting {}) sequence={} from: {}",
+            icmp.identifier,
+            program_state.identifier,
+            icmp.sequence,
+            remote_addr
+        ));
     }
-    match program_state.remove_sequence(icmp.sequence)
-    {
-        Some(original_instant) =>
-        {
+    match program_state.remove_sequence(icmp.sequence) {
+        Some(original_instant) => {
             let data_bytes = buf.len() - ICMPV4_PKT_BUF_SIZE;
             let remote_ip = IpAddr::V4(remote_sock.as_socket_ipv4().unwrap().ip().to_owned());
-            program_state.output_ping( data_bytes, &program_state.host_addr.ip(), &remote_ip, original_instant, icmp.sequence, ip.ttl );
+            program_state.output_ping(
+                data_bytes,
+                &program_state.host_addr.ip(),
+                &remote_ip,
+                original_instant,
+                icmp.sequence,
+                ip.ttl,
+            );
             program_state.responded_successfully += 1;
-        },
-        _ => log::error!( "got unexpected ICMP echo sequence {} from: {}", icmp.sequence, remote_addr ),
+        }
+        _ => log::error!(
+            "got unexpected ICMP echo sequence {} from: {}",
+            icmp.sequence,
+            remote_addr
+        ),
     }
 
     Ok(())
@@ -114,48 +134,74 @@ pub(crate) fn ipv4_handle_icmp_error(
     ip: &crate::ipv4::Ipv4PacketHeaderV4,
     buf: &[u8],
     icmp: &IcmpPacketHeaderV4,
-    program_state: &mut crate::IcmpProgramState) -> anyhow::Result<()>
-{
+    program_state: &mut crate::IcmpProgramState,
+) -> anyhow::Result<()> {
     // see if there's an encapsulated ICMP packet in this frame (unreachable/exceeded are encapsulated by an upstream gateway/router) //
 
     // encapsulated ip packet //
     let (_, buf_ip_encapsulated) = buf.split_at(ICMPV4_PKT_BUF_SIZE);
-    let ip_encapsulated: crate::ipv4::Ipv4PacketHeaderV4 = bincode::deserialize(buf_ip_encapsulated)?;
-    log::debug!( "got ip encapsulated packet: {:?}", &ip_encapsulated );
+    let ip_encapsulated: crate::ipv4::Ipv4PacketHeaderV4 =
+        bincode::deserialize(buf_ip_encapsulated)?;
+    log::debug!("got ip encapsulated packet: {:?}", &ip_encapsulated);
     let ip_encapsulated_version = (ip_encapsulated.header_length_and_version >> 4) & 0x0F;
-    let data_encapsulated_offset = (ip_encapsulated.header_length_and_version & 0x0F) * crate::ipv4::IPV4_PKT_OCTET_SIZE as u8;
-    if ip_encapsulated_version != crate::ipv4::D_IP_IP_VERSION_4 && data_encapsulated_offset != crate::ipv4::IPV4_PKT_BUF_SIZE as u8
+    let data_encapsulated_offset =
+        (ip_encapsulated.header_length_and_version & 0x0F) * crate::ipv4::IPV4_PKT_OCTET_SIZE as u8;
+    if ip_encapsulated_version != crate::ipv4::D_IP_IP_VERSION_4
+        && data_encapsulated_offset != crate::ipv4::IPV4_PKT_BUF_SIZE as u8
     {
-        return Err(anyhow::anyhow!("Invalid IPv4 encapsulated in ICMP error packet!"));
+        return Err(anyhow::anyhow!(
+            "Invalid IPv4 encapsulated in ICMP error packet!"
+        ));
     }
 
     // encapsulated icmp packet //
     let (_, buf_icmp_encapsulated) = buf_ip_encapsulated.split_at(data_encapsulated_offset.into());
     let icmp_encapsulated: IcmpPacketHeaderV4 = bincode::deserialize(buf_icmp_encapsulated)?;
-    log::debug!( "got icmp encapsulated packet: {:?}", &icmp_encapsulated );
+    log::debug!("got icmp encapsulated packet: {:?}", &icmp_encapsulated);
 
     // only regular output should be handled here... CSV just times out in the main loop //
     let remote_ip = IpAddr::V4(remote_sock.as_socket_ipv4().unwrap().ip().to_owned());
-    if icmp_encapsulated.identifier == program_state.identifier
-    {
-        if !matches!( program_state.output, crate::cli::OutputMode::CSV )
-        {
+    if icmp_encapsulated.identifier == program_state.identifier {
+        if !matches!(program_state.output, crate::cli::OutputMode::CSV) {
             // if regular output is being used then icmp errors are reported so we can dequeue them if we're tracking the sequence... otherwise let it timeout //
 
             // dequeue //
             let original_instant = program_state.remove_sequence(icmp_encapsulated.sequence);
 
             // report on the encapsulated packet //
-            match icmp.r#type
-            {
-                D_IPV4_ICMP_TYPE_UNREACHABLE => program_state.output_error( &program_state.host_addr.ip(), &remote_ip, original_instant, icmp_encapsulated.sequence, ip.ttl, "host unreachable".to_owned() ),
-                D_IPV4_ICMP_TYPE_EXCEEDED => program_state.output_error( &program_state.host_addr.ip(), &remote_ip, original_instant, icmp_encapsulated.sequence, ip.ttl, "time to live exceeded".to_owned() ),
-                _ => log::error!( "got unexpected ICMP echo identifier {} sequence {} from: {}", icmp_encapsulated.identifier, icmp_encapsulated.sequence, remote_ip ),
+            match icmp.r#type {
+                D_IPV4_ICMP_TYPE_UNREACHABLE => program_state.output_error(
+                    &program_state.host_addr.ip(),
+                    &remote_ip,
+                    original_instant,
+                    icmp_encapsulated.sequence,
+                    ip.ttl,
+                    "host unreachable".to_owned(),
+                ),
+                D_IPV4_ICMP_TYPE_EXCEEDED => program_state.output_error(
+                    &program_state.host_addr.ip(),
+                    &remote_ip,
+                    original_instant,
+                    icmp_encapsulated.sequence,
+                    ip.ttl,
+                    "time to live exceeded".to_owned(),
+                ),
+                _ => log::error!(
+                    "got unexpected ICMP echo identifier {} sequence {} from: {}",
+                    icmp_encapsulated.identifier,
+                    icmp_encapsulated.sequence,
+                    remote_ip
+                ),
             };
         }
-    }else
-    {
-        log::error!( "got unexpected ICMP echo identifier {} (was expecting {}) sequence={} from: {}", icmp.identifier, program_state.identifier, icmp.sequence, remote_ip );
+    } else {
+        log::error!(
+            "got unexpected ICMP echo identifier {} (was expecting {}) sequence={} from: {}",
+            icmp.identifier,
+            program_state.identifier,
+            icmp.sequence,
+            remote_ip
+        );
     }
     Ok(())
 }
