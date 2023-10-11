ChromaxS/challenge-icmprs

commit 511a1f2df879e6ad06a1f35e9ee1b899b48bbe7b

move echo request tick to separate function and improve program flow by moving common operations to program_stat impl

src/ipv4/icmp.rs
@@ -11,7 +11,7 @@ use anyhow;
 use bincode;
 use serde::{Serialize, Deserialize};
 use socket2::SockAddr;
-use std::time::Instant;
+use std::net::IpAddr;
 
 
 // ICMPv4 header size //
@@ -94,57 +94,18 @@ pub(crate) fn ipv4_handle_icmp_echo_reply(
     {
         return Err(anyhow::anyhow!( "got unexpected ICMP echo identifier {} (was expecting {}) sequence={} from: {}", icmp.identifier, program_state.identifier, icmp.sequence, remote_addr ));
     }
-    let now = Instant::now();
-    let mut found_sequence = false;
-    // this isn't the most efficient but we're not working with a large list of packets and even then so, it'd have to be hundreds of thousands or so //
-    // to be really bad //
-    program_state.sent.retain_mut(|sent|
+    match program_state.remove_sequence(icmp.sequence)
     {
-        // see if this is the packet we're looking for //
-        if sent.sequence != icmp.sequence
+        Some(original_instant) =>
         {
-            return true;
-        }
-
-        found_sequence = true;
-        if !program_state.quiet
-        {
-            match program_state.output
-            {
-                crate::cli::OutputMode::Default | crate::cli::OutputMode::Regular =>
-                {
-                    // regular output //
-                    match icmp.r#type
-                    {
-                        D_IPV4_ICMP_TYPE_ECHO_REPLY =>
-                        {
-                            // normal reply //
-                            let request_millis = (now - sent.instant).as_secs_f64() * 1000.0;
-                            let data_bytes = buf.len() - ICMPV4_PKT_BUF_SIZE;
-                            println!( "{} bytes from {}: icmp_seq={} ttl={}: {:.3} ms", data_bytes, remote_addr.ip(), sent.sequence, ip.ttl, request_millis );
-                        },
-                        _ => println!( "Reply from {}: icmp_seq={}: unknown code received", remote_addr.ip(), sent.sequence ),
-                    }
-                },
-                crate::cli::OutputMode::CSV =>
-                {
-                    // REQUIREMENT: IPv4,icmp_sequence_number,elapsed_time_in_microseconds
-                    let elapsed_in_microseconds = (now - program_state.started).as_micros();
-                    println!( "{},{},{}", remote_addr.ip(), sent.sequence, elapsed_in_microseconds );
-                },
-            }
-        }
-
-        if icmp.r#type == D_IPV4_ICMP_TYPE_ECHO_REPLY
-        {
+            let data_bytes = buf.len() - ICMPV4_PKT_BUF_SIZE;
+            let remote_ip = IpAddr::V4(remote_sock.as_socket_ipv4().unwrap().ip().to_owned());
+            program_state.output_ping( data_bytes, &program_state.host_addr.ip(), &remote_ip, original_instant, icmp.sequence, ip.ttl );
             program_state.responded_successfully += 1;
-        }
-        return false;
-    });
-    if !found_sequence
-    {
-        log::error!( "got unexpected ICMP echo sequence {} from: {}", icmp.sequence, remote_addr );
+        },
+        _ => log::error!( "got unexpected ICMP echo sequence {} from: {}", icmp.sequence, remote_addr ),
     }
+
     Ok(())
 }
 
@@ -172,48 +133,29 @@ pub(crate) fn ipv4_handle_icmp_error(
     let (_, buf_icmp_encapsulated) = buf_ip_encapsulated.split_at(data_encapsulated_offset.into());
     let icmp_encapsulated: IcmpPacketHeaderV4 = bincode::deserialize(buf_icmp_encapsulated)?;
     log::debug!( "got icmp encapsulated packet: {:?}", &icmp_encapsulated );
-    let remote_addr = remote_sock.as_socket_ipv4().unwrap();
+
+    // only regular output should be handled here... CSV just times out in the main loop //
+    let remote_ip = IpAddr::V4(remote_sock.as_socket_ipv4().unwrap().ip().to_owned());
     if icmp_encapsulated.identifier == program_state.identifier
     {
-        // report on the encapsulated packet //
-        match icmp.r#type
+        if !matches!( program_state.output, crate::cli::OutputMode::CSV )
         {
-            D_IPV4_ICMP_TYPE_UNREACHABLE =>
-            {
-                if !program_state.quiet && (matches!( program_state.output, crate::cli::OutputMode::Default ) || matches!( program_state.output, crate::cli::OutputMode::Regular ))
-                {
-                    println!( "Reply from {}: icmp_seq={} ttl={}: host unreachable", remote_addr.ip(), icmp_encapsulated.sequence, ip.ttl );
-                }
-            },
-            D_IPV4_ICMP_TYPE_EXCEEDED =>
-            {
-                if !program_state.quiet && (matches!( program_state.output, crate::cli::OutputMode::Default ) || matches!( program_state.output, crate::cli::OutputMode::Regular ))
-                {
-                    println!( "Reply from {}: icmp_seq={} ttl={}: time to live exceeded", remote_addr.ip(), icmp_encapsulated.sequence, ip.ttl );
-                }
-            },
-            _ => log::error!( "got unexpected ICMP echo identifier {} sequence {} from: {}", icmp_encapsulated.identifier, icmp_encapsulated.sequence, remote_addr ),
-        }
+            // if regular output is being used then icmp errors are reported so we can dequeue them if we're tracking the sequence... otherwise let it timeout //
 
-        // if regular output is being used then icmp errors are reported so we can dequeue them if we're tracking the sequence... otherwise let it timeout //
-        if matches!( program_state.output, crate::cli::OutputMode::Default ) || matches!( program_state.output, crate::cli::OutputMode::Regular )
-        {
-            program_state.sent.retain_mut(|sent|
+            // dequeue //
+            let original_instant = program_state.remove_sequence(icmp_encapsulated.sequence);
+
+            // report on the encapsulated packet //
+            match icmp.r#type
             {
-                // see if this is the packet we're looking for //
-                if sent.sequence != icmp_encapsulated.sequence
-                {
-                    return true;
-                }
-
-                log::debug!( "dequeued sent tracking sequence ID: {}", sent.sequence );
-
-                return false;
-            });
+                D_IPV4_ICMP_TYPE_UNREACHABLE => program_state.output_error( &program_state.host_addr.ip(), &remote_ip, original_instant, icmp_encapsulated.sequence, ip.ttl, "host unreachable".to_owned() ),
+                D_IPV4_ICMP_TYPE_EXCEEDED => program_state.output_error( &program_state.host_addr.ip(), &remote_ip, original_instant, icmp_encapsulated.sequence, ip.ttl, "time to live exceeded".to_owned() ),
+                _ => log::error!( "got unexpected ICMP echo identifier {} sequence {} from: {}", icmp_encapsulated.identifier, icmp_encapsulated.sequence, remote_ip ),
+            };
         }
     }else
     {
-        log::error!( "got unexpected ICMP echo identifier {} (was expecting {}) sequence={} from: {}", icmp.identifier, program_state.identifier, icmp.sequence, remote_addr );
+        log::error!( "got unexpected ICMP echo identifier {} (was expecting {}) sequence={} from: {}", icmp.identifier, program_state.identifier, icmp.sequence, remote_ip );
     }
     Ok(())
 }

src/main.rs
@@ -15,7 +15,7 @@ use log;
 use rand::{self, Rng};
 use socket2::SockAddr;
 use std::env;
-use std::net::{SocketAddr, ToSocketAddrs};
+use std::net::{SocketAddr, IpAddr, ToSocketAddrs};
 use std::process::exit;
 use std::time::{Duration, Instant};
 use tokio::signal;
@@ -30,6 +30,7 @@ mod ipv4;
 const IPV4_MAX_PACKET_SIZE: usize = 4096;
 
 
+#[derive(Clone)]
 pub(crate) struct IcmpEchoRequest
 {
     pub instant: Instant,
@@ -43,6 +44,7 @@ pub(crate) struct IcmpProgramState
     pub quiet: bool,
     pub output: cli::OutputMode,
 
+    pub host_addr: SocketAddr,
     pub identifier: u16,
     pub sequence: u16,
 
@@ -176,9 +178,6 @@ async fn main_loop(
     // shutdown notification //
     let shutdown_notification = shutdown_notify.notified();
 
-    // timeout //
-    let timeout = Duration::from_millis(args.timeout);
-
     // program state to pass into everything //
     let mut rng = rand::thread_rng();
     let mut program_state = IcmpProgramState
@@ -187,6 +186,7 @@ async fn main_loop(
         responded_successfully: 0,
         quiet: args.quiet,
         output: args.output.clone(),
+        host_addr: host_addr.to_owned(),
 
         // make a random identifier //
         identifier: rng.gen(),
@@ -241,75 +241,7 @@ async fn main_loop(
                 break;
             },
             // echo requests //
-            _ = echo_interval.tick() =>
-            {
-                // clear out timed out requests //
-                // this isn't too efficient (at larger amounts of pings, like hundreds of thousands) but it gives us feedback at the right time //
-                // ie 5 seconds after each 200ms interval //
-                let now = Instant::now();
-                let remote_addr = host_addr.ip();
-                program_state.sent.retain_mut(|sent|
-                {
-                    // if the timeout duration hasn't been hit then skip this //
-                    if now - sent.instant < timeout
-                    {
-                        return true;
-                    }
-
-                    if !args.quiet
-                    {
-                        match program_state.output
-                        {
-                            crate::cli::OutputMode::Default | crate::cli::OutputMode::Regular =>
-                            {
-                                // regular output //
-                                let request_millis = (now - sent.instant).as_secs_f64() * 1000.0;
-                                println!( "Reply from {}: icmp_seq={}: {:.3} ms -- timed out waiting for response", host_ip.ip(), sent.sequence, request_millis );
-                            },
-                            crate::cli::OutputMode::CSV =>
-                            {
-                                // REQUIREMENT: If the reply times out, use -1 for the elapsed_time_in_microseconds field.
-                                //              IPv4,icmp_sequence_number,elapsed_time_in_microseconds
-                                println!( "{},{},{}", remote_addr, sent.sequence, -1 );
-                            }
-                        }
-                    }
-
-                    return false;
-                });
-
-                // make sure we don't send more echo requests than requested //
-                if program_state.sent_total >= args.count
-                {
-                    if program_state.sent.is_empty()
-                    {
-                        // we're done //
-                        log::debug!( "echo request count, {}, reached on echo request tick and no outstanding replies -- done", args.count );
-                        shutdown_notify.notify_waiters();
-                    }
-                    continue;
-                }
-                log::debug!( "echo request {} tick!", program_state.sent_total );
-
-                if host_addr.is_ipv4()
-                {
-                    // send //
-                    let buffer = ipv4::icmp::ipv4_icmp_create_echo_request( program_state.identifier, program_state.sequence, args.size )?;
-                    socket.send(&buffer)?;
-
-                    // track //
-                    program_state.sent.push(IcmpEchoRequest
-                    {
-                        instant: Instant::now(),
-                        sequence: program_state.sequence,
-                    });
-
-                    // and increment the sequence for next go //
-                    program_state.sequence = program_state.sequence.wrapping_add(1);
-                }
-
-                program_state.sent_total += 1;
-            },
+            _ = echo_interval.tick() => { icmp_send_tick( args, shutdown_notify, &host_ip, socket, &mut program_state )?; },
             // icmp incoming (echo replies) //
             received = socket.recv_from(&mut buf_recv) =>
             {
@@ -317,7 +249,7 @@ async fn main_loop(
                 {
                     Ok(received) =>
                     {
-                        if host_addr.is_ipv4()
+                        if program_state.host_addr.is_ipv4()
                         {
                             match ipv4::ipv4_handle_packet( &received.1, &buf_recv, &mut program_state )
                             {
@@ -354,4 +286,178 @@ async fn main_loop(
     }
 
     Ok(program_state.responded_successfully == program_state.sent_total)
+}
+
+fn icmp_send_tick(
+  args: &cli::Args,
+  shutdown_notify: &Notify,
+  host_ip: &SocketAddr,
+  socket: &ip::IcmpSocket,
+  program_state: &mut IcmpProgramState
+) -> anyhow::Result<bool>
+{
+    // maybe send an ICMP packet -- returns bool whether on was sent or not //
+
+    // clear out timed out requests //
+    // this isn't too efficient (at larger amounts of pings, like hundreds of thousands) but it gives us feedback at the right time //
+    // ie 5 seconds after each 200ms interval //
+    let now = Instant::now();
+    let remote_addr = program_state.host_addr.ip();
+    let timeout = Duration::from_millis(args.timeout);
+    let mut timed_out_requests = Vec::<IcmpEchoRequest>::new();
+    program_state.sent.retain_mut(|sent|
+    {
+        // if the timeout duration hasn't been hit then skip this //
+        if now - sent.instant < timeout
+        {
+            return true;
+        }
+
+        timed_out_requests.push(sent.clone());
+
+        false
+    });
+    for timed_out_request in timed_out_requests.iter()
+    {
+        program_state.output_error( &remote_addr, &host_ip.ip(), Some(timed_out_request.instant), timed_out_request.sequence, 0, "timed out waiting for response".to_string() );
+    }
+
+    // make sure we don't send more echo requests than requested //
+    if program_state.sent_total >= args.count
+    {
+        if !program_state.has_sequences()
+        {
+            // we're done //
+            log::debug!( "echo request count, {}, reached on echo request tick and no outstanding replies -- done", args.count );
+            shutdown_notify.notify_waiters();
+        }
+        return Ok(false);
+    }
+    log::debug!( "echo request {} tick!", program_state.sent_total );
+
+    if program_state.host_addr.is_ipv4()
+    {
+        // send //
+        let buffer = ipv4::icmp::ipv4_icmp_create_echo_request( program_state.identifier, program_state.sequence, args.size )?;
+        socket.send(&buffer)?;
+
+        // track //
+        program_state.sent.push(IcmpEchoRequest
+        {
+            instant: Instant::now(),
+            sequence: program_state.sequence,
+        });
+
+        // and increment the sequence for next go //
+        program_state.sequence = program_state.sequence.wrapping_add(1);
+    }
+
+    program_state.sent_total += 1;
+
+    Ok(true)
+}
+
+
+impl IcmpProgramState
+{
+    pub fn has_sequences(&self) -> bool
+    {
+        // returns whether there's any sequences being tracked //
+        !self.sent.is_empty()
+    }
+
+    pub fn remove_sequence(&mut self, sequence: u16) -> Option<Instant>
+    {
+        // removes the specified sequence and returns the original Instant it was sent if found //
+
+        // this isn't the most efficient but we're not working with a large list of packets and even then so, it'd have to be hundreds of thousands or so //
+        // to be really bad //
+        let mut found_sequence = None;
+        self.sent.retain_mut(|sent|
+        {
+            // see if this is the packet we're looking for //
+            if sent.sequence != sequence
+            {
+                return true;
+            }
+
+            log::debug!( "dequeued sent tracking sequence ID: {}", sent.sequence );
+            found_sequence = Some(sent.instant);
+
+            false
+        });
+        found_sequence
+    }
+
+    pub fn output_csv(&self, host_ip: &IpAddr, instant_original: Option<Instant>, sequence: u16)
+    {
+        // output CSV format (if not quiet) //
+        if self.quiet
+        {
+            return;
+        }
+        if let Some(instant_original) = instant_original
+        {
+            // REQUIREMENT: IPv4,icmp_sequence_number,elapsed_time_in_microseconds
+            let elapsed_in_microseconds = (Instant::now() - instant_original).as_micros();
+            println!( "{},{},{}", host_ip, sequence, elapsed_in_microseconds );
+        }else
+        {
+            // REQUIREMENT: If the reply times out, use -1 for the elapsed_time_in_microseconds field.
+            println!( "{},{},-1", host_ip, sequence );
+        }
+    }
+
+    pub fn output_error(&self, host_ip: &IpAddr, remote_ip: &IpAddr, instant_original: Option<Instant>, sequence: u16, ttl: u8, error: String)
+    {
+        // outputs an error (if not set to quiet) //
+        if self.quiet
+        {
+            return;
+        }
+        match self.output
+        {
+            crate::cli::OutputMode::Default | crate::cli::OutputMode::Regular =>
+            {
+                // regular output //
+                match instant_original
+                {
+                    Some(instant_original) =>
+                    {
+                        let request_millis = (Instant::now() - instant_original).as_secs_f64() * 1000.0;
+                        println!( "Reply from {}: icmp_seq={} ttl={}: {:.3} ms -- {}", remote_ip, sequence, ttl, request_millis, error );
+                    },
+                    _ => println!( "Reply from {}: icmp_seq={} ttl={}: {}", remote_ip, sequence, ttl, error ),
+                }
+            },
+            crate::cli::OutputMode::CSV =>
+            {
+                // all errors are attributed to the host IP not the received IP //
+                self.output_csv( host_ip, None, sequence );
+            }
+        }
+    }
+
+    pub fn output_ping(&self, data_bytes: usize, host_ip: &IpAddr, remote_ip: &IpAddr, instant_original: Instant, sequence: u16, ttl: u8)
+    {
+        // outputs a normal ping (if not quiet) //
+        if self.quiet
+        {
+            return;
+        }
+        match self.output
+        {
+            crate::cli::OutputMode::Default | crate::cli::OutputMode::Regular =>
+            {
+                // regular output //
+                let request_millis = (Instant::now() - instant_original).as_secs_f64() * 1000.0;
+                println!( "{} bytes from {}: icmp_seq={} ttl={}: {:.3} ms", data_bytes, remote_ip, sequence, ttl, request_millis );
+            },
+                crate::cli::OutputMode::CSV =>
+            {
+                // all errors are attributed to the host IP not the received IP //
+                self.output_csv( host_ip, Some(instant_original), sequence );
+            }
+        }
+    }
 }

commit deb1bf76e7d567dd1ca01e00b3f197bc91cb8c7d

fix regression introduced by the refactor from commit 511a1f2 which changed CSV ping out time from micros elapsed since program start to micros of request round trip time

src/ipv4/icmp.rs
@@ -113,6 +113,7 @@ pub(crate) fn ipv4_handle_icmp_echo_reply(
                 data_bytes,
                 &program_state.host_addr.ip(),
                 &remote_ip,
+                program_state.started,
                 original_instant,
                 icmp.sequence,
                 ip.ttl,

src/main.rs
@@ -412,7 +412,7 @@ impl IcmpProgramState {
         &self,
         host_ip: &IpAddr,
         remote_ip: &IpAddr,
-        instant_original: Option<Instant>,
+        instant_echo_request: Option<Instant>,
         sequence: u16,
         ttl: u8,
         error: String,
@@ -424,10 +424,10 @@ impl IcmpProgramState {
         match self.output {
             crate::cli::OutputMode::Default | crate::cli::OutputMode::Regular => {
                 // regular output //
-                match instant_original {
-                    Some(instant_original) => {
+                match instant_echo_request {
+                    Some(instant_echo_request) => {
                         let request_millis =
-                            (Instant::now() - instant_original).as_secs_f64() * 1000.0;
+                            (Instant::now() - instant_echo_request).as_secs_f64() * 1000.0;
                         println!(
                             "Reply from {}: icmp_seq={} ttl={}: {:.3} ms -- {}",
                             remote_ip, sequence, ttl, request_millis, error
@@ -451,7 +451,8 @@ impl IcmpProgramState {
         data_bytes: usize,
         host_ip: &IpAddr,
         remote_ip: &IpAddr,
-        instant_original: Instant,
+        instant_program_started: Instant,
+        instant_echo_request: Instant,
         sequence: u16,
         ttl: u8,
     ) {
@@ -462,7 +463,7 @@ impl IcmpProgramState {
         match self.output {
             crate::cli::OutputMode::Default | crate::cli::OutputMode::Regular => {
                 // regular output //
-                let request_millis = (Instant::now() - instant_original).as_secs_f64() * 1000.0;
+                let request_millis = (Instant::now() - instant_echo_request).as_secs_f64() * 1000.0;
                 println!(
                     "{} bytes from {}: icmp_seq={} ttl={}: {:.3} ms",
                     data_bytes, remote_ip, sequence, ttl, request_millis
@@ -470,7 +471,7 @@ impl IcmpProgramState {
             }
             crate::cli::OutputMode::CSV => {
                 // all errors are attributed to the host IP not the received IP //
-                self.output_csv(host_ip, Some(instant_original), sequence);
+                self.output_csv(host_ip, Some(instant_program_started), sequence);
             }
         }
     }